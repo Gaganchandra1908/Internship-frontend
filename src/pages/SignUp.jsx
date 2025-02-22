a2528466218406bea91bd1cdcc741c1acaee3c58
Gaganchandra1908 <163104742+Gaganchandra1908@users.noreply.github.com>
2025-02-22 21:34:06 +0530

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -32,12 +32,13 @@ const SignUp = () => {
     formData.append("address", address);
     formData.append("role", role);
     formData.append("profileImage", profileImage);
-    role === "Auctioneer" &&
-      (formData.append("bankAccountName", bankAccountName),
-      formData.append("bankAccountNumber", bankAccountNumber),
-      formData.append("bankName", bankName),
-      formData.append("easypaisaAccountNumber", easypaisaAccountNumber),
-      formData.append("paypalEmail", paypalEmail));
+    if (role === "Auctioneer") {
+      formData.append("bankAccountName", bankAccountName);
+      formData.append("bankAccountNumber", bankAccountNumber);
+      formData.append("bankName", bankName);
+      formData.append("easypaisaAccountNumber", easypaisaAccountNumber);
+      formData.append("paypalEmail", paypalEmail);
+    }
     dispatch(register(formData));
   };
 
@@ -45,7 +46,7 @@ const SignUp = () => {
     if (isAuthenticated) {
       navigateTo("/");
     }
-  }, [dispatch, loading, isAuthenticated]);
+  }, [dispatch, loading, isAuthenticated, navigateTo]);
 
   const imageHandler = (e) => {
     const file = e.target.files[0];
@@ -58,182 +59,161 @@ const SignUp = () => {
   };
 
   return (
-    <>
-      <section className="w-full min-h-screen flex items-center justify-center bg-gray-100 pl-[320px]">
-
-        <div className="bg-white mx-auto w-full h-auto px-2 flex flex-col gap-4 items-center py-4 justify-center rounded-md">
-          <h1
-            className={`text-[#d6482b] text-2xl font-bold mb-2 min-[480px]:text-4xl md:text-6xl xl:text-7xl 2xl:text-8xl`}
-          >
-            Register
-          </h1>
-          <form
-            className="flex flex-col gap-5 w-full"
-            onSubmit={handleRegister}
-          >
-            <p className="font-semibold text-xl md:text-2xl">
-              Personal Details
-            </p>
-            <div className="flex flex-col gap-4 sm:flex-row">
-              <div className="flex flex-col sm:flex-1">
-                <label className="text-[16px] text-stone-600">Full Name</label>
+    <section className="w-full min-h-screen flex items-center justify-center bg-background pl-[320px]">
+      <div className="bg-card mx-auto w-full h-auto px-4 flex flex-col gap-4 items-center py-6 justify-center rounded-md shadow-lg">
+        <h1 className="text-primary text-3xl font-bold mb-2 sm:text-4xl md:text-5xl lg:text-6xl">
+          Register
+        </h1>
+        <form className="flex flex-col gap-5 w-full" onSubmit={handleRegister}>
+          <p className="font-semibold text-xl md:text-2xl">Personal Details</p>
+          <div className="flex flex-col gap-4 sm:flex-row">
+            <div className="flex flex-col sm:flex-1">
+              <label className="text-base text-muted">Full Name</label>
+              <input
+                type="text"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+                className="text-base py-2 bg-transparent border-b border-border focus:outline-none"
+              />
+            </div>
+            <div className="flex flex-col sm:flex-1">
+              <label className="text-base text-muted">Email</label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="text-base py-2 bg-transparent border-b border-border focus:outline-none"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 sm:flex-row">
+            <div className="flex flex-col sm:flex-1">
+              <label className="text-base text-muted">Phone</label>
+              <input
+                type="number"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                className="text-base py-2 bg-transparent border-b border-border focus:outline-none"
+              />
+            </div>
+            <div className="flex flex-col sm:flex-1">
+              <label className="text-base text-muted">Address</label>
+              <input
+                type="text"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                className="text-base py-2 bg-transparent border-b border-border focus:outline-none"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 sm:flex-row">
+            <div className="flex flex-col sm:flex-1">
+              <label className="text-base text-muted">Role</label>
+              <select
+                value={role}
+                onChange={(e) => setRole(e.target.value)}
+                className="text-base py-2 bg-transparent border-b border-border focus:outline-none"
+              >
+                <option value="">Select Role</option>
+                <option value="Auctioneer">Auctioneer</option>
+                <option value="Bidder">Bidder</option>
+              </select>
+            </div>
+            <div className="flex flex-col sm:flex-1">
+              <label className="text-base text-muted">Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="text-base py-2 bg-transparent border-b border-border focus:outline-none"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col sm:flex-1 gap-2">
+            <label className="text-base text-muted">Profile Image</label>
+            <div className="flex items-center gap-3">
+              <img
+                src={profileImagePreview ? profileImagePreview : "/imageHolder.jpg"}
+                alt="Profile Preview"
+                className="w-14 h-14 rounded-full"
+              />
+              <input type="file" onChange={imageHandler} />
+            </div>
+          </div>
+          <div className="flex flex-col gap-4">
+            <label className="font-semibold text-xl md:text-2xl flex flex-col">
+              Payment Method Details{" "}
+              <span className="text-xs text-muted">
+                Fill Payment Details Only If you are registering as an Auctioneer
+              </span>
+            </label>
+            <div className="flex flex-col gap-2">
+              <label className="text-base text-muted">Bank Details</label>
+              <div className="flex flex-col gap-1 sm:flex-row sm:gap-4">
+                <select
+                  value={bankName}
+                  onChange={(e) => setBankName(e.target.value)}
+                  className="text-base py-2 bg-transparent border-b border-border focus:outline-none sm:flex-1"
+                  disabled={role === "Bidder"}
+                >
+                  <option value="">Select Your Bank</option>
+                  <option value="Meezan Bank">Meezan Bank</option>
+                  <option value="UBL">UBL</option>
+                  <option value="HBL">HBL</option>
+                  <option value="Allied Bank">Allied Bank</option>
+                </select>
                 <input
                   type="text"
-                  value={userName}
-                  onChange={(e) => setUserName(e.target.value)}
-                  className="text-[16px] py-2 bg-transparent border-b-[1px] border-b-stone-500 focus:outline-none"
+                  value={bankAccountNumber}
+                  placeholder="IBAN / IFSC"
+                  onChange={(e) => setBankAccountNumber(e.target.value)}
+                  className="text-base py-2 bg-transparent border-b border-border focus:outline-none sm:flex-1"
+                  disabled={role === "Bidder"}
                 />
-              </div>
-              <div className="flex flex-col sm:flex-1">
-                <label className="text-[16px] text-stone-600">Email</label>
                 <input
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="text-[16px] py-2 bg-transparent border-b-[1px] border-b-stone-500 focus:outline-none"
+                  type="text"
+                  value={bankAccountName}
+                  placeholder="Bank Account UserName"
+                  onChange={(e) => setBankAccountName(e.target.value)}
+                  className="text-base py-2 bg-transparent border-b border-border focus:outline-none sm:flex-1"
+                  disabled={role === "Bidder"}
                 />
               </div>
             </div>
-            <div className="flex flex-col gap-4 sm:flex-row">
-              <div className="flex flex-col sm:flex-1">
-                <label className="text-[16px] text-stone-600">Phone</label>
+            <div>
+              <label className="text-base text-muted font-semibold">
+                Easypaisa And Paypal Details
+              </label>
+              <div className="flex flex-col gap-1 sm:flex-row sm:gap-4">
                 <input
                   type="number"
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
-                  className="text-[16px] py-2 bg-transparent border-b-[1px] border-b-stone-500 focus:outline-none"
+                  value={easypaisaAccountNumber}
+                  placeholder="Easypaisa Account Number"
+                  onChange={(e) => setEasypaisaAccountNumber(e.target.value)}
+                  className="text-base py-2 bg-transparent border-b border-border focus:outline-none sm:flex-1"
+                  disabled={role === "Bidder"}
                 />
-              </div>
-              <div className="flex flex-col sm:flex-1">
-                <label className="text-[16px] text-stone-600">Address</label>
                 <input
-                  type="text"
-                  value={address}
-                  onChange={(e) => setAddress(e.target.value)}
-                  className="text-[16px] py-2 bg-transparent border-b-[1px] border-b-stone-500 focus:outline-none"
+                  type="email"
+                  value={paypalEmail}
+                  placeholder="Paypal Email"
+                  onChange={(e) => setPaypalEmail(e.target.value)}
+                  className="text-base py-2 bg-transparent border-b border-border focus:outline-none sm:flex-1"
+                  disabled={role === "Bidder"}
                 />
               </div>
             </div>
-            <div className="flex flex-col gap-4 sm:flex-row">
-              <div className="flex flex-col sm:flex-1">
-                <label className="text-[16px] text-stone-600">Role</label>
-                <select
-                  value={role}
-                  onChange={(e) => setRole(e.target.value)}
-                  className="text-[16px] py-2 bg-transparent border-b-[1px] border-b-stone-500 focus:outline-none"
-                >
-                  <option value="">Select Role</option>
-                  <option value="Auctioneer">Auctioneer</option>
-                  <option value="Bidder">Bidder</option>
-                </select>
-              </div>
-              <div className="flex flex-col sm:flex-1">
-                <label className="text-[16px] text-stone-600">Password</label>
-                <input
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="text-[16px] py-2 bg-transparent border-b-[1px] border-b-stone-500 focus:outline-none"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col sm:flex-1 gap-2">
-              <label className="text-[16px] text-stone-600">
-                Profile Image
-              </label>
-              <div className="flex items-center gap-3">
-                <img
-                  src={
-                    profileImagePreview
-                      ? profileImagePreview
-                      : "/imageHolder.jpg"
-                  }
-                  alt="profileImagePreview"
-                  className="w-14 h-14 rounded-full"
-                />
-                <input type="file" onChange={imageHandler} />
-              </div>
-            </div>
-            <div className="flex flex-col gap-4">
-              <label className="font-semibold text-xl md:2xl flex flex-col">
-                Payment Method Details{" "}
-                <span className="text-[12px] text-stone-500">
-                  Fill Payment Details Only If you are registering as an
-                  Auctioneer
-                </span>
-              </label>
-              <div className="flex flex-col gap-2">
-                <label className="text-[16px] text-stone-500">
-                  Bank Details
-                </label>
-                <div className="flex flex-col gap-1 sm:flex-row sm:gap-4">
-                  <select
-                    value={bankName}
-                    onChange={(e) => setBankName(e.target.value)}
-                    className="text-[16px] py-2 bg-transparent border-b-[1px] border-b-stone-500 focus:outline-none sm:flex-1"
-                    disabled={role === "Bidder"}
-                  >
-                    <option value="">Select Your Bank</option>
-                    <option value="Meezan Bank">Meezan Bank</option>
-                    <option value="UBL">UBL</option>
-                    <option value="HBL">HBL</option>
-                    <option value="Allied Bank">Allied Bank</option>
-                  </select>
-                  <input
-                    type="text"
-                    value={bankAccountNumber}
-                    placeholder="IBAN / IFSC"
-                    onChange={(e) => setBankAccountNumber(e.target.value)}
-                    className="text-[16px] py-2 bg-transparent border-b-[1px] border-b-stone-500 focus:outline-none sm:flex-1"
-                    disabled={role === "Bidder"}
-                  />
-                  <input
-                    type="text"
-                    value={bankAccountName}
-                    placeholder="Bank Account UserName"
-                    onChange={(e) => setBankAccountName(e.target.value)}
-                    className="text-[16px] py-2 bg-transparent border-b-[1px] border-b-stone-500 focus:outline-none sm:flex-1"
-                    disabled={role === "Bidder"}
-                  />
-                </div>
-              </div>
-              <div>
-                <label className="text-[16px] text-stone-600 font-semibold">
-                  Easypaisa And Paypal Details
-                </label>
-                <div className="flex flex-col gap-1 sm:flex-row sm:gap-4">
-                  <input
-                    type="number"
-                    value={easypaisaAccountNumber}
-                    placeholder="Easypaisa Account Number"
-                    onChange={(e) => setEasypaisaAccountNumber(e.target.value)}
-                    className="text-[16px] py-2 bg-transparent border-b-[1px] border-b-stone-500 focus:outline-none sm:flex-1"
-                    disabled={role === "Bidder"}
-                  />
-                  <input
-                    type="email"
-                    value={paypalEmail}
-                    placeholder="Paypal Email"
-                    onChange={(e) => setPaypalEmail(e.target.value)}
-                    className="text-[16px] py-2 bg-transparent border-b-[1px] border-b-stone-500 focus:outline-none sm:flex-1"
-                    disabled={role === "Bidder"}
-                  />
-                </div>
-              </div>
-            </div>
-
-            <button
-              className="bg-[#d6482b] w-[420px] font-semibold hover:bg-[#b8381e] transition-all duration-300 text-xl py-2 px-4 rounded-md text-white mx-auto lg:w-[640px] my-4"
-              type="submit"
-              disabled={loading}
-            >
-              {loading && "Registering..."}
-              {!loading && "Register"}
-            </button>
-          </form>
-        </div>
-      </section>
-    </>
+          </div>
+          <button
+            className="btn btn-primary w-[420px] lg:w-[640px] text-xl py-2 my-4 mx-auto"
+            type="submit"
+            disabled={loading}
+          >
+            {loading ? "Registering..." : "Register"}
+          </button>
+        </form>
+      </div>
+    </section>
   );
 };
 
